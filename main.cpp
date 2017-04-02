73ded3c41dcd630dac948ffc9f71069020b05ff6
jdotglove <glover.jarod@gmail.com>
2017-04-01 20:56:25 -0500

    the main of the program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Main Program                                                                *
+ * For Pacman Game (COMP 11 Project 1 | 2016 Spring)                           *
+ ******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#include "game.h"
+
+int main()
+{
+        Game g;
+        g.print_manual();
+        g.run();
+}
